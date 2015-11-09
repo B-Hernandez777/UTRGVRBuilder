e0f841ae42bcc7e109f8fdafa05ffba6bec4b464
Huriel Hernandez <Huriel.hdz@gmail.com>
2015-11-08 19:19:49 -0600

    Added event handlers that display the textfield label only after the textfield has been entered.  Textfields get validated to see if they are empty.  IF they are empty then the error messages displays before user can press login.

--- src/LoginController.java
+++ src/LoginController.java
@@ -10,6 +10,8 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -20,13 +22,22 @@ public class LoginController implements Initializable
 {
 	@FXML Button loginButton;
 	@FXML TextField emailTextField;
-	
+	@FXML Label emailLabel;
+	@FXML Label emailErrorLabel;
+	@FXML Label passwordLabel;
+	@FXML Label passwordErrorLabel;
+	@FXML PasswordField passwordField;
 	
 	
 	@FXML private void loginButtonClicked(ActionEvent event) throws IOException
 	{
 		System.out.println(emailTextField.getText());
 		
+		//Validate email text field is not empty
+		if(emailTextField.getText().isEmpty())
+		{}
+		else
+		{
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Main.fxml"));
 		Parent root = (Pane) fxmlLoader.load();
 		Scene scene = new Scene(root);
@@ -38,7 +49,7 @@ public class LoginController implements Initializable
 		stage.setTitle("Main2");
 		stage.show();
 		((Node)(event.getSource())).getScene().getWindow().hide();
-		
+		}
 		
 		
 	}
@@ -46,7 +57,57 @@ public class LoginController implements Initializable
 	@Override
 	public void initialize(URL location, ResourceBundle resources)
 	{
+		//Experiment to try and validate on the spot
+		//Hides email Label until EmailField is entered
+		emailTextField.setOnKeyPressed(e->
+		{
+			emailLabel.setVisible(true);
+			emailErrorLabel.setVisible(false);
+		});
+		emailTextField.focusedProperty().addListener(e->
+		{
+			if(emailTextField.isFocused()== false)
+			{
+				if(emailTextField.getText().isEmpty())
+				{
+					emailLabel.setVisible(false);
+					emailErrorLabel.setVisible(true);
+					emailTextField.getStyleClass().add("error");
+				}			
+			}
+			else
+			if(emailTextField.getText().isEmpty())
+				{
+					emailLabel.setVisible(false);
+					emailErrorLabel.setVisible(false);
+					emailTextField.getStyleClass().remove("error");
+				}			
+		});
 		
+		passwordField.setOnKeyPressed(e->
+		{
+			passwordLabel.setVisible(true);
+			passwordErrorLabel.setVisible(false);
+		});
+		passwordField.focusedProperty().addListener(e->
+		{
+			if(passwordField.isFocused()== false)
+			{
+				if(passwordField.getText().isEmpty())
+				{
+					passwordLabel.setVisible(false);
+					passwordErrorLabel.setVisible(true);
+					passwordField.getStyleClass().add("error");
+				}			
+			}
+			else
+			if(passwordField.getText().isEmpty())
+				{
+					passwordLabel.setVisible(false);
+					passwordErrorLabel.setVisible(false);
+					passwordField.getStyleClass().remove("error");
+				}			
+		});
 	}
 
 }
