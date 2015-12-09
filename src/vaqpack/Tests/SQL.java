2185df893ca6666077514fde14c87b2e4cf503dd
abel <abelluna@172.28.7.228>
2015-12-09 15:24:17 -0600

    Fixed SQL.java to work in multiple classes.

--- src/vaqpack/Tests/SQL.java
+++ src/vaqpack/Tests/SQL.java
@@ -9,64 +9,70 @@ public class SQL {
 	final static String DB = "jdbc:mysql://localhost:3306/$users";
 	final static String User = "student";
 	final static String Password = "pass";
-	final static String Table_info = "`info_table`";
 	final static String Table_user = "`user_table`";// contains email, password, java_objects.
-													
-	private String user_email = "JohnDoe@hotmail.com";
-
+	
 	/**
-	 * This function requires the user_email to already be specified in the
-	 * class in order to work. Upon login or registration, the user_email should have been set.
-	 * If you're testing, set the user_email in SQL.java on line 18 to whatever it is you're using. 
+	 * This function requires the email of the user to be passed as a String
+	 * in order to work. 
 	 * 
 	 * @return This returns the vaqPack Object from the DB.
-	 * @throws SQLException
 	 */
-	public Object getVaqPack() throws SQLException {
+	public static Object getVaqPack(String email){
+		Object vaqPack = null;
+		try{
 		Connection conn = DriverManager.getConnection(DB, User, Password);
 		PreparedStatement getDB = conn
-				.prepareStatement("SELECT * from" + Table_user + "WHERE email='" + user_email + "'");
+				.prepareStatement("SELECT * from" + Table_user + "WHERE email='" + email + "'");
 		ResultSet rs = getDB.executeQuery();
 		rs.next();
-		Object vaqPack = rs.getBlob(4);
+		vaqPack = rs.getBlob(4);
 		rs.close();
 		getDB.close();
 		System.out.println("SQL getVaqPack() completed");
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+			e.getMessage();
+		}
 		return vaqPack;
 	}
 
 	/**
-	 * This will update the DB with whatever is in the VaqPack Object.
+	 * This will update the DB with whatever is in the VaqPack Object to whichever email you specify.
 	 * 
 	 * @param vaqPack
 	 *            This should be the array list object you're passing to
 	 *            it.
-	 * @throws SQLException
+	 * @param email This is the email of the user that you want to update the object to.
+	 * 
 	 */
-	public void updateDB(Object vaqPack) throws SQLException {
+	public void updateDB(Object vaqPack, String email){
+		try{
 		Connection conn = DriverManager.getConnection(DB, User, Password);
 		PreparedStatement updateDB = conn
 				.prepareStatement("UPDATE" + Table_user + "SET java_objects=? WHERE email = ?");
 		updateDB.setObject(1, vaqPack);
-		updateDB.setString(2, this.user_email);
+		updateDB.setString(2, email);
 		updateDB.executeUpdate();
 		updateDB.close();
 		conn.close();
 		System.out.println("updateDB() completed");
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+			e.getMessage();
+		}
 	}
-
 	/**
 	 * Confirms if the email and password match in the DB.
 	 * 
-	 * Requires 2 strings to be passed to this function.
+	 * Requires the email and password to be strings when passed to this function.
 	 * 
 	 * @param email
-	 *            The email the user provides as string.
 	 * @param password
-	 *            The password the user provides as string.
-	 * @throws SQLException
 	 */
-	public void confirmLogin(String email, String password) throws SQLException {
+	public void confirmLogin(String email, String password){
+		try{
 		Connection conn = DriverManager.getConnection(DB, User, Password);
 		ResultSet userSearch = conn.createStatement().executeQuery(
 				"SELECT * from   " + Table_user + "  WHERE email = '" + email + "' AND password = '" + password + "'");
@@ -74,12 +80,44 @@ public class SQL {
 			System.out.println("ERROR: Email NOT found!");
 			// Tell user to register.
 		} else {
-			this.user_email = email;
-
+			// User has successfully logged in,
 			// Set the scene to the next screen here.
 		}
 		userSearch.close();
 		conn.close();
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+			e.getMessage();
+		}
+	}
+	
+	/**
+	 * Searches for the email that is passed to this function as a String in the DB. 
+	 *
+	 * @param email This is the email of the user you want to check exists.
+	 * @return This returns a boolean, true or false.
+	 */
+	public static boolean userExists(String email){
+		Boolean user = null;
+		try{
+		Connection conn = DriverManager.getConnection(DB, User, Password);
+		ResultSet userSearch = conn.createStatement().executeQuery(
+				"SELECT * from   " + Table_user + "  WHERE email = '" + email + "'");
+		if (!userSearch.next()) {
+			System.out.println("Email not found!:: from userExists();");
+			user = false;
+		} else {
+			user = true;
+		}
+		userSearch.close();
+		conn.close();
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+			e.getMessage();
+		}
+		return user;
 	}
 
 	/**
@@ -92,28 +130,33 @@ public class SQL {
 	 *            The email the user provides as string.
 	 * @param password
 	 *            The password the user provides as string. 
-	 * @throws SQLException
+	 * @param vaqPack
+	 * 			The object you want to pass to the DB.
 	 */
-	public void registerUser(String email, String password) throws SQLException {
+	public void registerUser(String email, String password, Object vaqPack){
+		try{
 		Connection conn = DriverManager.getConnection(DB, User, Password);
 		ResultSet userSearch = conn.createStatement()
 				.executeQuery("SELECT * from   " + Table_user + "  WHERE email = '" + email + "' ");
 
 		if (!userSearch.next()) {
 			PreparedStatement addDB = conn
-					.prepareStatement("INSERT INTO " + Table_user + "(email, password) VALUE (?,?)");
+					.prepareStatement("INSERT INTO " + Table_user + "(email, password, java_objects) VALUE (?,?,?)");
 			addDB.setString(1, email);
 			addDB.setString(2, password);
+			addDB.setObject(3, vaqPack);
 			addDB.executeUpdate();
 			addDB.close();
-			this.user_email = email;
 		} else {
-			System.out.println("User already exists.");
+			System.out.println("Did not register user, user already exists :: from registerUser()");
 			// Tell user to reset password or something.
 		}
 		userSearch.close();
 		conn.close();
-
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+			e.getMessage();
+		}
 	}
-
 }
