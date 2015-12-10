1a015e6a3c2597d910268e76c96a30fe287a1527
Brandon H <kol4050@gmail.com>
2015-12-09 20:01:44 -0600

    Fix XML output

--- src/vaqpack/model/resume/Resume.java
+++ src/vaqpack/model/resume/Resume.java
@@ -6,115 +6,85 @@ import java.util.Arrays;
 
 import javafx.collections.ObservableList;
 
-public class Resume implements Serializable
-{
-	private static final long serialVersionUID = 1L;
-	
-	private Personal personal;
-	private Objective objective;
+public class Resume implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Personal personal;
+    private Objective objective;
     private ArrayList<Experience> experienceList;
     private ArrayList<Education> educationList;
     private ArrayList<Achievement> achievementList;
     private ArrayList<Skill> skillList;
     private ResumeStyle style;
+
+    public Resume(String email) {
+        personal = new Personal(email);
+    }
     
-    public Resume(String email)
-	{
-    	personal = new Personal(email);
-	}
 
+    public Personal getPersonal() {
+        return personal;
+    }
 
+    public void setPersonal(Personal personal) {
+        this.personal = personal;
+    }
 
-	public Personal getPersonal()
-	{
-		return personal;
-	}
+    public Objective getObjective() {
+        return objective;
+    }
 
+    public void setObjective(Objective objective) {
+        this.objective = objective;
+    }
 
-	public void setPersonal(Personal personal)
-	{
-		this.personal = personal;
-	}
+    public String toString() {
+        return String.format("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"+"<resume>" + "\n%s%s\t"
+                + "<experience>\n%s\t</experience>\n\t"
+                + "<education>\n%s\t</education>\n\t"
+                + "<achievement>\n%s\t</achievement>\n\t"
+                + "<skill>\n%s\t</skill>\n"
+                + "</resume>", personal, objective, getExperienceList(), getEducationList(), getAchievementList(), getSkillList());
+    }
 
+    public ResumeStyle getStyle() {
+        return style;
+    }
 
-	public Objective getObjective()
-	{
-		return objective;
-	}
+    public void setStyle(ResumeStyle style) {
+        this.style = style;
+    }
 
+    public ArrayList<Experience> getExperienceList() {
+        return experienceList;
+    }
 
-	public void setObjective(Objective objective)
-	{
-		this.objective = objective;
-	}
-	
-	public String toString()
-	{
-		return String.format("<resume>" + "\n%s%s\t"
-				+ "<experience>\n%s\t</experience>\n\t"
-				+ "<education>\n%s\t</education>\n\t"
-				+ "<achievement>\n%s\t</achievement>\n\t"
-				+ "<skill>\n%s\t</skill>\n"
-				+ "</resume>"
-				,personal, objective, getExperienceList(), getEducationList(), getAchievementList(), getSkillList() );
-	}
+    public void setExperienceList(ArrayList<Experience> experienceList) {
+        this.experienceList = experienceList;
+    }
 
-	public ResumeStyle getStyle()
-	{
-		return style;
-	}
+    public ArrayList<Education> getEducationList() {
+        return educationList;
+    }
 
+    public void setEducationList(ArrayList<Education> educationList) {
+        this.educationList = educationList;
+    }
 
-	public void setStyle(ResumeStyle style)
-	{
-		this.style = style;
-	}
+    public ArrayList<Achievement> getAchievementList() {
+        return achievementList;
+    }
 
-	public ArrayList<Experience> getExperienceList()
-	{
-		return experienceList;
-	}
+    public void setAchievementList(ArrayList<Achievement> achievementList) {
+        this.achievementList = achievementList;
+    }
 
+    public ArrayList<Skill> getSkillList() {
+        return skillList;
+    }
 
-	public void setExperienceList(ArrayList<Experience> experienceList)
-	{
-		this.experienceList = experienceList;
-	}
-
-
-	public ArrayList<Education> getEducationList()
-	{
-		return educationList;
-	}
-
-	public void setEducationList(ArrayList<Education> educationList)
-	{
-		this.educationList = educationList;
-	}
-
-
-
-	public ArrayList<Achievement> getAchievementList()
-	{
-		return achievementList;
-	}
-
-
-
-	public void setAchievementList(ArrayList<Achievement> achievementList)
-	{
-		this.achievementList = achievementList;
-	}
-
-
-
-	public ArrayList<Skill> getSkillList()
-	{
-		return skillList;
-	}
-
-	public void setSkillList(ArrayList<Skill> skillList)
-	{
-		this.skillList = skillList;
-	}
+    public void setSkillList(ArrayList<Skill> skillList) {
+        this.skillList = skillList;
+    }
 }
