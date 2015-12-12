2125cc8cf0b0f460b5a73fe953becbd96290762b
Brandon H <kol4050@gmail.com>
2015-12-11 20:09:30 -0600

    added some steps for export

--- src/vaqpack/controller/ExportController.java
+++ src/vaqpack/controller/ExportController.java
@@ -89,5 +89,31 @@ public class ExportController implements Initializable
 		}
 
 	}
+        @FXML
+        public void exportButtonClicked()
+        {
+            if(resumeCheckBox.isSelected() )
+            {
+                //get the resume xml
+                // so either we can make the xml from the resume elements here 
+                // or have it save some where else
+                
+                // then we send the xml and the selected style which will be set from the xsl i will upload
+                // then send to the html maker function in resume styles and html name that the user can select or
+                // it can be a preset name like resumewebsite.html
+                // and the returns the html name
+                
+                // then with the htmlname ( which is the html file ) to the pdf function under export
+                // and it will save the pdf in the set location or I suppose file so i can add the file parameter
+            }
+            // get the selected info / array
+            // call the style function
+            // so if the user selected the style 1 then the html function will be 
+            // given the selected xml file and the selected style and the name of the html
+            // the default name could be resumewebsite.html
+            // then send to the html maker function in resume style( the function can be moved ) 
+            // it returns the html and with that you can make the pdf and keep it in the same saved location
+            
+        }
 
 }
