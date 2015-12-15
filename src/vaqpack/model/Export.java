d12aef9a79a6193f73ef1eef63f3e443fbc5b234
Brandon H <kol4050@gmail.com>
2015-12-14 22:08:18 -0600

    added pdf combiner function

--- src/vaqpack/model/Export.java
+++ src/vaqpack/model/Export.java
@@ -5,13 +5,21 @@
  */
 package vaqpack.model;
 
+import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.pdf.PdfContentByte;
+import com.lowagie.text.pdf.PdfImportedPage;
+import com.lowagie.text.pdf.PdfReader;
+import com.lowagie.text.pdf.PdfWriter;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Serializable;
+import java.util.List;
 
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
@@ -71,6 +79,46 @@ public class Export implements Serializable {
             System.out.println("Error in the html to pdf converter");
         }
         return pdfFileName;
+    }
+    public static void combinePDFFiles(List<String> list, String outName) throws DocumentException, IOException 
+    {
+        OutputStream out=null;
+        Document document=null;
+        PdfWriter writer =null;
+        FileInputStream in=null;
+        try {
+            // Resulting pdf
+             out = new FileOutputStream(new File(outName));
+             document = new Document();
+             writer = PdfWriter.getInstance(document, out);
+            document.open();
+            PdfContentByte cb = writer.getDirectContent();
+            for (String inFile : list) {
+                in = new FileInputStream(new File(inFile));
+                PdfReader reader = new PdfReader(in);
+                for (int i = 1; i <= reader.getNumberOfPages(); i++) {
+                    document.newPage();
+                    //import the page from source pdf
+                    PdfImportedPage page = writer.getImportedPage(reader, i);
+                    //add the page to the destination pdf
+                    cb.addTemplate(page, 0, 0);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally
+        {
+            out.flush();
+            document.close();
+            out.close();
+            in.close();
+        }
+
     }
 
 }
