0e19b369901dd3f1c9d507ac2c61aaad5cc49272
Huriel Hernandez <Huriel.hdz@gmail.com>
2015-12-14 19:23:16 -0600

    Added images to cover letter preview

--- src/vaqpack/controller/CoverLetterStyleController.java
+++ src/vaqpack/controller/CoverLetterStyleController.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -29,7 +30,7 @@ public class CoverLetterStyleController implements Initializable
 	@FXML VBox styleCard;
 	@FXML StackPane styleListCard;
 	@FXML Button saveButton;
-
+	@FXML ImageView imageStyle;
 	
 	@FXML ListView<CoverLetterStyle> styleListView;
 	
